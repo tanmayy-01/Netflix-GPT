6b148be74e0ea4cd24b96b607d2f2ab65224957c
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-01-07 10:31:48 +0530

    Added background and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-500">
-      <p>Welcome to Netflix</p>
-    </div>
+    <Body />
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+         alt="bg-image" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
