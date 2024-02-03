fd9bf479d4402a22fa2057527836b3e0025f7811
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-02-03 12:50:37 +0530

    Updated the header of the browse page and implement the logout feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,22 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidate } from '../utils/validate';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
 
+    const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handlSubmitButton = () => {
         console.log(email.current.value);
@@ -20,11 +26,36 @@ const Login = () => {
         console.log(message);
         setErrorMessage(message);
         if(message) return;
+
+
         if(!isSignInForm) {
-          createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+          createUserWithEmailAndPassword(
+            auth,
+            email.current.value, 
+            password.current.value
+          )
           .then((userCredential) => {
             const user = userCredential.user;
+            updateProfile(user, {
+              displayName: name.current.value,
+              photoURL: "https://cdn2.vectorstock.com/i/1000x1000/25/91/buddhist-monk-character-design-vector-4612591.jpg"
+            }).then(() => {
+
+              const {uid, email, displayName, photoURL} = auth.currentUser;
+              dispatch(addUser({
+                uid:uid,
+                email:email,
+                displayName: displayName,
+                photoURL: photoURL
+              
+              }));
+              navigate('/browse');
+            }).catch((error) => {
+              setErrorMessage(error);
+            });
+            
             console.log(user);
+            navigate('/browse');
           })
           .catch((error) => {
             const errorCode = error.code;
@@ -38,6 +69,7 @@ const Login = () => {
           
             const user = userCredential.user;
             console.log(user);
+            navigate('/browse');
           })
           .catch((error) => {
             const errorCode = error.code;
@@ -62,6 +94,7 @@ const Login = () => {
       <form onSubmit={e => e.preventDefault()} className = "w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm && (<input
+            ref={name}
             type='text'
             placeholder='Full Name'
             className='p-4 my-4 w-full bg-gray-700 rounded-lg' 
